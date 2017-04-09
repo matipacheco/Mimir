d0da15c4ece673c0229c61b2e724e7065ae7e845
Mati <matipacheco@gmail.com>
2017-04-09 17:14:42 -0300

    por si hace falta...

--- the_well.py
+++ the_well.py
@@ -2,6 +2,15 @@ import settings
 from peewee import *
 from MySQLdb import *
 
+# CREATE TABLE dollar_records (
+#     id INT NOT NULL AUTO_INCREMENT,
+#     amount FLOAT,
+#     shift  FLOAT,
+#     date   DATE,
+#    	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+#    	PRIMARY KEY (id)
+# );
+
 db = MySQLDatabase( settings.DATABASE_NAME,
 										user 		= settings.DATABASE_USER,
 										passwd 	= settings.DATABASE_PASS,
